94cb4c707b9f7430448c210d76ec48cf62623e68
atacanjacques <atacan.jacques@gmail.com>
2017-04-10 18:30:17 +0200

    CRUD of Genres

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.2'
+gem 'rails', '~> 5.0.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
@@ -19,7 +19,7 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
-# gem 'therubyracer', platforms: :ruby
+gem 'therubyracer', platforms: :ruby
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
@@ -47,3 +47,8 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# Custom gem
+#gem 'sass-rails'
+gem 'bootstrap-sass'
+gem 'autoprefixer-rails'
---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,55 @@
+class GenresController < ApplicationController
+  before_action :set_genre, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @genres = Genre.all
+  end
+
+  def show
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def edit
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+
+    respond_to do |format|
+      if @genre.save
+        format.html { redirect_to @genre, notice: 'Le genre a été ajouter avec succès.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @genre.update(genre_params)
+        format.html { redirect_to @genre, notice: 'Le genre a été modifier avec succès.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @genre.destroy
+    respond_to do |format|
+      format.html { redirect_to genres_url, notice: 'Le genre a été supprimer avec succès.' }
+    end
+  end
+
+  private
+    def set_genre
+      @genre = Genre.find(params[:id])
+    end
+
+    def genre_params
+      params.require(:genre).permit(:nom)
+    end
+end
