d0402c7dbb64c6f7be0d3ef1ad49240bbe88f167
atacanjacques <atacan.jacques@gmail.com>
2017-07-11 23:24:37 +0200

    added input validations && ActiveRecord erros translation

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,3 +1,5 @@
 class Channel < ApplicationRecord
   has_many :tv_shows
+
+  validates :name, presence: true
 end
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -3,4 +3,9 @@ class Episode < ApplicationRecord
 
   has_many :user_episode
   has_many :users, through: :user_episode
+
+  validates :season, presence: true
+  validates :episode, presence: true
+  validates :title,  presence: true
+  validates :tv_show_id,  presence: true
 end
